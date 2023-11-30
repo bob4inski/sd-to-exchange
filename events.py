abdaf243c4dbf1ef8921abd4b108c41017007088
bob4inski <robertstryzhak@gmail.com>
2023-11-30 11:51:40 +0300

    rewrite get_objects

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import logging
 import mysql.connector
 import pandas as pd
 import numpy as np
@@ -36,7 +37,7 @@ def get_events_from_bd():
     return df
 
 
-def get_object_from_bd(object_id: str):
+def get_objects_from_bd():
 
     mydb = mysql.connector.MySQLConnection(
         user=os.environ["DB_USER"] or "user",
@@ -48,15 +49,16 @@ def get_object_from_bd(object_id: str):
 
     cur = mydb.cursor()
     querry = """
-                SELECT name FROM 
+                SELECT id, name FROM 
                     custom_field_enumerations AS cfe 
-                WHERE id = %s 
+                where custom_field_id = 110
             """
 
-    cur.execute(querry, [object_id])
+    cur.execute(querry)
     response = cur.fetchall()
 
-    return response[0][0]
+    response_dict = {object_id: value for object_id, value in response}
+    return response_dict
 
 def get_from_api(url: str, redmine_key: str):
 
@@ -70,7 +72,7 @@ def get_from_api(url: str, redmine_key: str):
     return response.json()
 
 
-def get_events_from_api():
+def get_events_from_api(objects: dict):
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     
     if os.path.exists(dotenv_path):
@@ -78,45 +80,56 @@ def get_events_from_api():
 
     url = os.environ['REDMINE_URL']
     key = os.environ['REDMINE_KEY']
+    
+    try:
+        logging.debug('try to get data')
+        data = get_from_api(url=url, redmine_key=key)
+    except Exception as ex:
+        logging.debug(ex)
 
-    data = get_from_api(url=url, redmine_key=key)
-    issues = data["issues"]
-    issues_list = []
+    if data["issues"]:
+        issues = data["issues"]
+        issues_list = []
 
-    for row in issues:
-        issue = {
-            "id": row["id"],
-            "subject": row["subject"],
-            "status_id": row["status"]["id"],
-            "start": "",
-            "finish": "",
-            "object": "None"
-            }
+        for row in issues:
+            issue = {
+                "id": row["id"],
+                "subject": row["subject"],
+                "status_id": row["status"]["id"],
+                "start": "",
+                "finish": "",
+                "object": "None"
+                }
 
-        for custom_field in row["custom_fields"]:
-            if custom_field["id"] == 146:
-                issue["start"] = custom_field["value"]
+            for custom_field in row["custom_fields"]:
+                if custom_field["id"] == 146:
+                    issue["start"] = custom_field["value"]
 
-            if custom_field["id"] == 147:
-                issue["finish"] = custom_field["value"]
+                if custom_field["id"] == 147:
+                    issue["finish"] = custom_field["value"]
 
-            if custom_field["id"] == 110:
-                issue["object"] = get_object_from_bd(custom_field["value"])
+                if custom_field["id"] == 110:
+                    issue["object"] = objects[int(custom_field["value"])]
 
-        issues_list.append(issue)
+            issues_list.append(issue)
 
-    df = pd.DataFrame(issues_list, columns=["id", "subject", "status_id", "start", "finish", "object"])
-    return df
+        df = pd.DataFrame(issues_list, columns=["id", "subject", "status_id", "start", "finish", "object"])
 
+        return df
+    else:
+        logging.debug("there is no issues")
+        logging.info("shutdown")
+        exit(1)
 
 def normalize():
-    
+
+    objects_from_bd = get_objects_from_bd()
+
     # events = get_events_from_bd()
-    events = get_events_from_api()
+    events = get_events_from_api(objects=objects_from_bd)
 
     events["start"].replace("", np.nan, inplace=True)
     events.dropna(subset=['start'], inplace=True)
 
     return events
-
 
