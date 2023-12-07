78be33295933df62953d8cc0acb25222c82db9b2
bob4inski <robertstryzhak@gmail.com>
2023-12-07 12:18:02 +0300

    change requests to redmine

--- events.py
+++ events.py
@@ -9,55 +9,58 @@ import os
 from conenctions.redmine import get_locations_from_db
 from conenctions.redmine import get_from_api
 from datetime import datetime, timedelta
+from redminelib import Redmine
 
 def get_events_from_api(locations: dict):
-    # dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    
-    # if os.path.exists(dotenv_path):
-    #     load_dotenv(dotenv_path)
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
 
-    # key = os.environ['REDMINE_KEY']
-    key = "54bd095f52e85f988860c6e2def8bf91b9177c7b"
+    key = os.environ['REDMINE_KEY']
     
     try:
         logging.debug('try to get data')
-        url = "https://sd.talantiuspeh.ru/issues.json?project_id=9&status_id=*&offset=0&limit=1000&per_page=1000;"
-        data = get_from_api(url=url, redmine_key=key)
-
+        redmine = Redmine("https://sd.talantiuspeh.ru/", key=key)
+        issues = redmine.issue.filter(
+                    project_id=9,
+                    limit=1000
+                )
     except Exception as ex:
         logging.debug(ex)
 
-    if data["issues"]:
-        issues = data["issues"]
+    if issues:
         issues_list = []
 
-        for row in issues:
-            issue = {
-                "id": row["id"],
-                "subject": row["subject"],
-                "status_id": row["status"]["id"],
+        for issue in issues:
+            issue_dict = {
+                "id": issue.id,
+                "subject": issue.subject,
+                "status": issue.status,
                 "start_time": "",
                 "finish_time": "",
                 "duration": "",
+                "close_code": "",
                 "location": "None"
                 }
 
-            for custom_field in row["custom_fields"]:
-                if custom_field["id"] == 24:
-                    issue["start_time"] = custom_field["value"]
-                
-                if custom_field["id"] == 110:
-                    issue["location"] = locations[int(custom_field["value"])]
+            for custom_field in issue.custom_fields:
+                if custom_field.id  == 24:
+                    issue_dict["start_time"] = custom_field.value
 
-                if custom_field["id"] == 115 and custom_field["value"]:
-                    issue["duration"] = int(custom_field["value"])
-                    # issue["finish_time"] = issue["start_time"] + timedelta(minutes=issue["duration"])
-                    issue["finish_time"] = str(datetime.strptime(str(issue["start_time"]), "%Y-%m-%d %H:%M:%S") + timedelta(minutes=issue["duration"]))
+                if custom_field.id  == 81:
+                    issue_dict["close_code"] = custom_field.value
+
+                if custom_field.id == 110:
+                    issue_dict["location"] = locations[int(custom_field.value)]
+
+                if custom_field.id == 115 and custom_field.value:
+                    issue_dict["duration"] = int(custom_field.value)
+                    issue_dict["finish_time"] = str(datetime.strptime(str(issue_dict["start_time"]), "%Y-%m-%d %H:%M:%S") + timedelta(minutes=issue_dict["duration"]))
             
-            if issue["finish_time"]:
-                issues_list.append(issue)
+            if issue_dict["finish_time"]:
+                issues_list.append(issue_dict)
                   
-        df = pd.DataFrame(issues_list, columns=["id", "subject", "status_id", "start_time", "finish_time","duration", "location"])
+        df = pd.DataFrame(issues_list, columns=["id", "subject", "status_id", "start_time", "finish_time","duration","close_code", "location"])
 
         return df
     else:
